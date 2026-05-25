8f4e0cc224f98e49a0305344f8ff5eff80a68c0c
K07-2p0 <adrianofsoliveira@gmail.com>
2026-05-25 12:18:51 +0100

    C4: remover correr.bat (substituido por java -jar target/c4.jar)

--- C4/correr.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: ============================================================
-::  correr.bat - Corre o programa C4 (Windows)
-::  Criptografia Aplicada | LSIRC 2025/2026
-:: ============================================================
-
-java -cp ".;lib/*" AssinarPDF
