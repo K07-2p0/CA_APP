d175edab4e6b93caa8ec46b8c5a093c60886b445
K07-2p0 <adrianofsoliveira@gmail.com>
2026-05-25 12:18:58 +0100

    C4: remover correr.sh (substituido por java -jar target/c4.jar)

--- C4/correr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# ============================================================
-#  correr.sh - Corre o programa C4
-#  Criptografia Aplicada | LSIRC 2025/2026
-# ============================================================
-
-JARS="lib/*"
-
-java -cp ".:$JARS" AssinarPDF
